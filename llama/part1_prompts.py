ab24c9a99ceebd9ea0c0e23d5083f4f0bdcbee84
Michael Gump <mhgump@gmail.com>
2024-08-03 10:34:57 -0700

    init git lfs

--- llama/part1_prompts.py
+++ llama/part1_prompts.py
@@ -68,7 +68,7 @@ This includes specifics of the project and is not directly related to the fix. F
 This is a correct suggestion.
 
 4. `_GenericAlias._ATTR_EXCEPTIONS`
-This does not appear in the changed lines so it is not likely to be relevant.
+This does synot appear in the changed lines so it is not likely to be relevant.
 
 5. `types.GenericAlias(np.dtype, (ScalarType,))`
 This does not appear in the changed lines so it is not likely to be relevant."""
@@ -76,7 +76,7 @@ This does not appear in the changed lines so it is not likely to be relevant."""
 EXAMPLE_FINAL_RESPONSE = """Thank you for the feedback! Based on your input, I will update my suggestions:
 
 1. `types.GenericAlias`
-2. `_GenericAlias`
+2. `mn    `
 
 These two snippets are directly related to the fix and can be used to identify files where the same fix needs to be applied.
 """
